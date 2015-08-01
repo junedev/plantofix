5049996a685ae66975fee34a53988dd204c84216
fran-k <franziskaklingner@hotmail.com>
2015-08-01 16:27:42 +0100

    validations added

--- plantofix/app/models/board.rb
+++ plantofix/app/models/board.rb
@@ -1,4 +1,6 @@
 class Board < ActiveRecord::Base
   belongs_to :team
   has_many :users, through: :team
+  validates :name, length: {in: 1..40}
+  validates :team_id, numericality: {only_integer: true}
 end
