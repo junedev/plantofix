2de7faf512ecff202257b054d69ff25370a431dd
fran-k <franziskaklingner@hotmail.com>
2015-08-01 11:44:22 +0100

    users, teams and boards created with rails g resource

--- plantofix/config/routes.rb
+++ plantofix/config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  resources :boards
+  resources :teams
+  resources :users
+  get 'session/new'
+
+  get 'session/create'
+
+  get 'session/destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
