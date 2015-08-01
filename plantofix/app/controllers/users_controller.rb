67f32194d84cf2e7e6207a48cc9b0104b4d9210f
fran-k <franziskaklingner@hotmail.com>
2015-08-01 12:46:50 +0100

    user controller set up

--- plantofix/app/controllers/users_controller.rb
+++ plantofix/app/controllers/users_controller.rb
@@ -1,6 +1,4 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate, only: [:show]
 
   # Profile page
   # GET /users/1
@@ -18,52 +16,41 @@ class UsersController < ApplicationController
   # Edit profile
   # GET /users/1/edit
   def edit
-    redirect_to login_path unless authenticate_user(@user)
+    @user = User.find(params[:id])
+    redirect_to root_path unless authenticate_user(@user)
   end
 
+  # Save new user from to database
   # POST /users
-  # POST /users.json
   def create
     @user = User.new(user_params)
-
     if @user.save
       session[:user_id] = @user.id
       redirect_to @user, notice: 'User was successfully created.'
-      else
-        render :new
-      end
+    else
+      render :new
+    end
   end
 
+  # Save edited profile to database
   # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    @user = User.new(user_params)
+    if @user.update(user_params)
+      redirect_to @user, notice: 'User was successfully updated.'
+    else
+      render :edit
     end
   end
 
   # DELETE /users/1
-  # DELETE /users/1.json
   def destroy
+    @user = User.new(user_params)
     @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to users_url, notice: 'User was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
-
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user).permit(:username, :avatar_url, :email, :password, :password_confirmation)
