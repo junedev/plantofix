e95f0b01ee863d53a0e8f1b5ca9ae949b54a99a2
fran-k <franziskaklingner@hotmail.com>
2015-08-02 16:41:20 +0100

    items can be dragged and sorted using sortable from jquery ui

--- plantofix/app/assets/javascripts/tasks.js
+++ plantofix/app/assets/javascripts/tasks.js
@@ -1,22 +1,23 @@
 $( function(){
+
   $(".task_edit").on("click", function(){
     event.stopPropagation();
     $(this).closest('.task_item_box').siblings('.task_edit_box').show();
     $(this).closest('.task_item_box').hide();
   });
-
+  $(".sortable").sortable({connectWith: ".sortable"});
 });
 
-function allowDrop(ev) {
-  ev.preventDefault();
-}
+// function allowDrop(ev) {
+//   ev.preventDefault();
+// }
 
-function drag(ev) {
-  ev.dataTransfer.setData("text", ev.target.id);
-}
+// function drag(ev) {
+//   ev.dataTransfer.setData("text", ev.target.id);
+// }
 
-function drop(ev) {
-  ev.preventDefault();
-  var data = ev.dataTransfer.getData("text");
-  ev.target.appendChild(document.getElementById(data));
-}
+// function drop(ev) {
+//   ev.preventDefault();
+//   var data = ev.dataTransfer.getData("text");
+//   ev.target.appendChild(document.getElementById(data));
+// }
