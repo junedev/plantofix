5ca3e9ee4f0daf029971e7b20e14873e1d58119e
fran-k <franziskaklingner@hotmail.com>
2015-08-05 11:03:00 +0100

    account details styled

--- plantofix/app/assets/javascripts/teams.js
+++ plantofix/app/assets/javascripts/teams.js
@@ -1,6 +1,6 @@
 $( function(){
   $(".add_team_button").on("click", function(){
     $(this).hide();
-    $(this).siblings('.add_team_form').show();
+    $(this).parent().siblings('.add_team_form').show();
   });
 });
