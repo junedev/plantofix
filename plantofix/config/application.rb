a80785785c2985697ac28e114a027b2e98b513c9
fran-k <franziskaklingner@hotmail.com>
2015-08-07 00:54:20 +0100

    commit before trying to push to heroku

--- plantofix/config/application.rb
+++ plantofix/config/application.rb
@@ -22,6 +22,7 @@ module Plantofix
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.assets.precompile += ["/fonts*"]
+    config.serve_static_assets = true
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
