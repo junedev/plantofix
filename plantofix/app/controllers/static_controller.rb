70351a90fdbe7ddb63b096f2e659ebad802b4c38
fran-k <franziskaklingner@hotmail.com>
2015-08-06 15:47:34 +0100

    error due to failed import from github is rescued

--- plantofix/app/controllers/static_controller.rb
+++ plantofix/app/controllers/static_controller.rb
@@ -11,21 +11,29 @@ class StaticController < ApplicationController
     position = 1
     username = params["github"]["username"]
     repo = params["github"]["repo"]
-    github = Github.new
-    issues = Github::Client::Issues.new
+    
     team = Team.find_private_team(current_user)
     board = team.boards.create!(name:repo)
     list = board.lists.create!(name:"ToDo")
     board.lists.create!(name:"In progress")
     board.lists.create!(name:"Done")
-    imported_data = issues.list(user: username, repo: repo, state: 'open')
-    if imported_data.length > 0
-      imported_data.each do |issue|
-        list.tasks.create!(name: issue["title"], color: "white", position: position += 1)
-      end
-      redirect_to board_path(board)
-    else
+
+    github = Github.new
+    issues = Github::Client::Issues.new
+    
+    begin
+      imported_data = issues.list(user: username, repo: repo, state: 'open')
+    rescue
       redirect_to "/import", alert: "Import not successful."
+    else
+      if imported_data.length > 0
+        imported_data.each do |issue|
+          list.tasks.create!(name: issue["title"], color: "white", position: position += 1)
+        end
+        redirect_to board_path(board)
+      else
+        redirect_to "/import", alert: "Import not successful."
+      end
     end
   end
 
