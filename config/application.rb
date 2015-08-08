5adb049785b4faad2f30e3d3b4eba3220f6eb48d
fran-k <franziskaklingner@hotmail.com>
2015-08-08 11:40:42 +0100

    logo layout for firefox fixes, font import changed

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,6 @@ module Plantofix
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.assets.precompile += ["/fonts*"]
     config.serve_static_assets = true
     config.active_record.raise_in_transactional_callbacks = true
   end
