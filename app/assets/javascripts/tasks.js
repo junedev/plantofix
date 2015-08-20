bdd3bc1ffca2acdfe8ad506020dfecfe5c50e6c9
fran-k <franziskaklingner@hotmail.com>
2015-08-20 20:54:40 +0100

    task boxes take color from boxes in popover, still some bug with selectors though

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -27,80 +27,90 @@ $( function(){
   $('.task_description_modal').on('shown.bs.modal', function () {
   });
 
-  // var pop_content = "<button class='btn color-btn first-color'></button>"
-  // var pop_content_1 = "<button class='btn color-btn' style='background-color: "
-  // var pop_content_2 = "'></button>";
-  // var task_colors = ['#A59E72', '#F7D488', '#FF9B71', '#E26D5A']
-  // for(var i=0; i<task_colors.length; i++){
-  //   pop_content += pop_content_1 + task_colors[i] + pop_content_2;
-  // };
+  var pop_content = "<button class='btn color-btn first-color'></button>"
+  var pop_content_1 = "<button class='btn color-btn' style='background-color: "
+  var pop_content_2 = "'></button>";
+  var task_colors = ['#A59E72', '#F7D488', '#FF9B71', '#E26D5A']
+  for(var i=0; i<task_colors.length; i++){
+    pop_content += pop_content_1 + task_colors[i] + pop_content_2;
+  };
   
-  // $('[data-toggle="popover"]').popover({
-  //   html: true,
-  //   content: pop_content,
-  //   container: "body"
-  // });
+  $("[data-toggle='popover']").popover({
+    html: true,
+    content: pop_content,
+    container: "body"
+  }).on("show.bs.popover", function(){
+    var currentBox = $(this).siblings(".task_item");
+    console.log("currentbox",currentBox);
+    $("body").on("click",".color-btn",function(){
+      console.log("this inside",this);
+      var color = $(this).css("backgroundColor");
+      currentBox.css("backgroundColor", color);
+    });
+  });
 
   // $("body").on("click",".color-btn",function(){
   //   var color = $(this).css("backgroundColor");
+  //   console.log(color);
   // });
 
-  $('.colorp').colorpicker({color: "#ffffff"}).on('changeColor', function(ev) {
-    $(this).siblings(".task_item").css("background-color", ev.color.toHex())
-  });
 
-  $('.colorp').colorpicker().on('hidePicker', function(ev) {
-    $(this).siblings(".task_edit_box").find(".new_color").val(ev.color.toHex());
-    var updateData = { task: { color: ev.color.toHex() }};
-    $.ajax({
-      url:'http://localhost:3000/tasks/'+$(this).closest("li").data().id,
-      type:'put',
-      dataType: 'json',
-      contentType: 'application/json',
-      data: JSON.stringify(updateData)
-    })
-  });
+  // $('.colorp').colorpicker({color: "#ffffff"}).on('changeColor', function(ev) {
+  //   $(this).siblings(".task_item").css("background-color", ev.color.toHex())
+  // });
 
-  $("body").on("click",".task_delete", function(){
-    var that = this;
-    event.preventDefault();
-    $.ajax({
-      url:'/tasks/'+$(this).data().id,
-      type:'delete'
-    }).done(function(){
-      $(that).closest("li").remove();   
-    });
-  });
+  // $('.colorp').colorpicker().on('hidePicker', function(ev) {
+  //   $(this).siblings(".task_edit_box").find(".new_color").val(ev.color.toHex());
+  //   var updateData = { task: { color: ev.color.toHex() }};
+  //   $.ajax({
+  //     url:'http://localhost:3000/tasks/'+$(this).closest("li").data().id,
+  //     type:'put',
+  //     dataType: 'json',
+  //     contentType: 'application/json',
+  //     data: JSON.stringify(updateData)
+  //   })
+  // });
 
-  $("body").on("submit", "form.new_task_item",function(){
-    var that = this;
-    event.preventDefault();
-    $.ajax({
-      url: $(this).attr("action"),
-      type:$(this).attr("method"),
-      dataType: 'html',
-      data: $(this).serialize()
-    }).done(function(result){
-      console.log("done reached");
-      $(that).siblings("ul").append(result);
-      $(that).find("input[name='task[name]']").val(null);
-    });
+$("body").on("click",".task_delete", function(){
+  var that = this;
+  event.preventDefault();
+  $.ajax({
+    url:'/tasks/'+$(this).data().id,
+    type:'delete'
+  }).done(function(){
+    $(that).closest("li").remove();   
   });
+});
 
-  $("body").on("submit","form.task_edit_box",function(){
-    var that = this;
-    event.preventDefault();
-    $.ajax({
-      url: $(this).attr("action"),
-      type: $(this).attr("method"),
-      dataType: 'json',
-      data: $(this).serialize()
-    }).done(function(result){
+$("body").on("submit", "form.new_task_item",function(){
+  var that = this;
+  event.preventDefault();
+  $.ajax({
+    url: $(this).attr("action"),
+    type:$(this).attr("method"),
+    dataType: 'html',
+    data: $(this).serialize()
+  }).done(function(result){
+    console.log("done reached");
+    $(that).siblings("ul").append(result);
+    $(that).find("input[name='task[name]']").val(null);
+  });
+});
+
+$("body").on("submit","form.task_edit_box",function(){
+  var that = this;
+  event.preventDefault();
+  $.ajax({
+    url: $(this).attr("action"),
+    type: $(this).attr("method"),
+    dataType: 'json',
+    data: $(this).serialize()
+  }).done(function(result){
     $(that).hide();
     $(that).siblings('.task_item').children("p").html(result.name);
     $(that).siblings('.task_item').show();
-    });
   });
+});
 
 });
 
